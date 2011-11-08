90fe5cbf82eafcb92a2fcb190f2686a4a656952c
Kevin Diep <kdiep@cs.utsa.edu>
2011-11-08 09:46:08 -0600

    Making sure all our spork stuff is saved

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
+require 'spork'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+Spork.prefork do
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -30,4 +32,5 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+end
 end
